94eba3ea1c21a1dea57d9fc08a409940d8fcf171
Walton Goga <waltongoga@gmail.com>
2021-06-25 09:52:56 +0300

    Singleton scope

--- src/main/java/AppConfig.java
+++ src/main/java/AppConfig.java
@@ -2,13 +2,16 @@ import com.walton.repository.HibernateSpeakerRepositoryImpl;
 import com.walton.repository.SpeakerRepository;
 import com.walton.service.SpeakerService;
 import com.walton.service.SpeakerServiceImpl;
+import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 
 @Configuration
 public class AppConfig {
 
     @Bean(name = "speakerService")
+    @Scope(value = BeanDefinition.SCOPE_SINGLETON)
     public SpeakerService getSpeakerService() {
         SpeakerServiceImpl service = new SpeakerServiceImpl(getSpeakerRepository());
         //service.setRepository(getSpeakerRepository());

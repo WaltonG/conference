f484355e03009fc118e2ba78b3195314c54399af
Walton Goga <waltongoga@gmail.com>
2021-06-27 19:58:00 +0300

    Stereotypes

--- src/main/java/com/walton/repository/HibernateSpeakerRepositoryImpl.java
+++ src/main/java/com/walton/repository/HibernateSpeakerRepositoryImpl.java
@@ -1,10 +1,12 @@
 package com.walton.repository;
 
 import com.walton.model.Speaker;
+import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Repository("speakerRepository")
 public class HibernateSpeakerRepositoryImpl implements SpeakerRepository {
     @Override
     public List<Speaker> findAll() {
